6521276a8795f73da49c8de0ce6832838d460aa9
wangjiong01 <wangjiong01@58.com>
2022-10-20 15:07:58 +0800

    【优化】格式化代码

--- bin/power_command.dart
+++ bin/power_command.dart
@@ -11,11 +11,12 @@ import 'commands/export_command.dart';
 ///
 /// @Description: run脚本入口
 void main(List<String> args) async {
-  final runner = CommandRunner('power_command', 'A useful command line tool for Dart')
-    ..addCommand(DeleteCommand())
-    ..addCommand(CleanCommand())
-    ..addCommand(PubGetCommand())
-    ..addCommand(PureCommand());
+  final runner =
+      CommandRunner('power_command', 'A useful command line tool for Dart')
+        ..addCommand(DeleteCommand())
+        ..addCommand(CleanCommand())
+        ..addCommand(PubGetCommand())
+        ..addCommand(PureCommand());
   await runner.run(args).catchError((error) {
     if (error is! UsageException) throw error;
     print(error);
