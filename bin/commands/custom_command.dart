d7c00bf3627d4f4618e4ac9e6afe9cbd190957aa
wangjiong01 <wangjiong01@58.com>
2022-10-27 20:28:37 +0800

    【功能】支持运行指定的路径脚本

--- bin/commands/custom_command.dart
+++ bin/commands/custom_command.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+
 import 'base_command.dart';
 
 /// FileName customer_command
@@ -17,7 +21,7 @@ class CustomCommand extends BaseCommand {
         _command = command;
 
   @override
-  String get description => "run a custom command";
+  String get description => "run a custom command or its own script";
 
   @override
   String get name => _name ?? "custom";
@@ -26,7 +30,23 @@ class CustomCommand extends BaseCommand {
   List<String> get aliases => _aliases ?? super.aliases;
 
   @override
-  Future<void> run([String? commands]) {
-    return super.run(commands ?? _command ?? argResults?.rest.join(" "));
+  Future<void> run([String? commands]) async {
+    final results = argResults?.rest;
+    // run through its own script
+    if (results?.length == 1 && FileSystemEntity.isFileSync(results!.single)) {
+      final path = results.single;
+      final process =
+          await Process.start(p.extension(path).substring(1), [path]);
+
+      stdout.addStream(process.stdout);
+      stderr.addStream(process.stderr);
+
+      await process.exitCode;
+
+      process.kill();
+      return;
+    }
+    // run custom command by shell
+    return super.run(commands ?? _command ?? results?.join(" "));
   }
 }
